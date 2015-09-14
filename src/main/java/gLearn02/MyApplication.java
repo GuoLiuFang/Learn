acdd038a1f6bb07f406ed5aa34597ddbb6770e27
LiuFangGuo <guoliufang@tigerjoys.com>
2015-09-14 12:20:16 +0800

    Legacy Mode version two

--- src/main/java/gLearn02/MyApplication.java
+++ src/main/java/gLearn02/MyApplication.java
@@ -4,7 +4,16 @@ package gLearn02;
  * Created by LiuFangGuo on 9/14/15.
  */
 public class MyApplication {
-    private EmailService emailService = new EmailService();
+    private EmailService emailService = null;
+
+    /**
+     * 版本2.0把变量移到构造函数中去
+     * @param emailService
+     */
+    public MyApplication(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     public void processMessages(String msg, String rec){
         /**
          * do some msg validation, manipulation logic
